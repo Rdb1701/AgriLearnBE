8c40f6558c1a4a946ae2c33c3df619f969c67ed9
Wybie <lyrncofyn@gmail.com>
2025-12-30 04:06:07 +0800

    add TaskFactory for generating task model instances

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'code' => strtoupper($this->faker->unique()->lexify('????_????')),
+            'name' => $this->faker->words(3, true),
+            'description' => $this->faker->sentence(),
+        ];
+    }
+}
