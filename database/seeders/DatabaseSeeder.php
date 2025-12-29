a9278ecdd2b8506856c43e4903dcafa9922f844d
Wybie <lyrncofyn@gmail.com>
2025-12-30 04:06:14 +0800

    add tasks to DatabaseSeeder for initial data population

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(20)->create();
+        // User::factory(20)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Task::factory()->create([
+            'code' => 'LAND_PREP',
+            'name' => 'Land Preparation',
+            'description' => 'Prepare the land for planting by tilling the soil,',
+        ]);
+
+        Task::factory()->create([
+            'code' => 'PLANT_CORN',
+            'name' => 'Plant Corn Seeds',
+            'description' => 'Plant corn seeds in the prepared soil.',
+        ]);
+
+        Task::factory()->create([
+            'code' => 'PLANT_RICE',
+            'name' => 'Plant Rice Seeds',
+            'description' => 'Plant rice seeds in the prepared soil.',
+        ]);
+
+        Task::factory()->create([
+            'code' => 'WATERING',
+            'name' => 'Watering',
+            'description' => 'Water the plants to ensure they have enough moisture to grow.',
+        ]);
+
+        Task::factory()->create([
+            'code' => 'HARVESTING',
+            'name' => 'Harvesting',
+            'description' => 'Harvest the mature crops from the field.',
+        ]);
+
+        Task::factory()->create([
+            'code' => 'HAVEST_CORN',
+            'name' => 'Harvest Corn',
+            'description' => 'Harvest the mature corn from the field.',
+        ]);
+
+        Task::factory()->create([
+            'code' => 'HARVEST_RICE',
+            'name' => 'Harvest Rice',
+            'description' => 'Harvest the mature rice from the field.',
+        ]);
     }
 }
